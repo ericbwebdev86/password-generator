12a6b4c0ff35687cd73f21da3490952ed2f64340
unknown <ericbwebdev86@gmail.com>
2021-12-10 20:57:50 -0600

    pw criteria variables, length prompt in generatePassword func

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
 // Assignment code here
+let special = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ":", ":", "'", ",", "<", ".", ">", "/", "?", "`", "~"];
+let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 
+function generatePassword() {
+  //character length prompt
+  let length = window.prompt("Please enter desired character length. Enter a number from 8 to 128.");
+  //while loop for password length validation
+  while (length <= 7 || length >= 129) {
+    // window.alert("Invalid entry!");
+    length = window.prompt("Please enter desired character length. Enter a number from 8 to 128.");
+  }
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");

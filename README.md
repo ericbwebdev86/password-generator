cb9c76b7704f8340ecff47c770e6599e10f5f3e6
unknown <ericbwebdev86@gmail.com>
2021-12-11 19:04:43 -0600

    filled out readme, fixed length while loop validation

--- README.md
+++ README.md
@@ -1,1 +1,35 @@
-# Password Generator Starter Code
+# Password Generator
+
+## purpose
+A website that allows the user to generate a randomized password with specified criteria through browser window prompts.
+
+## Built with
+* HTML
+* CSS
+* JavaScript
+
+## Screenshots
+### Landing Page
+![image](https://user-images.githubusercontent.com/87142377/145694982-53fff7b9-92af-486a-a65f-6b209b93e9ec.png)
+
+### Prompts
+#### Password length prompt
+![image](https://user-images.githubusercontent.com/87142377/145695142-c5a964d0-6065-4ff4-9e09-e8d6949f2d08.png)
+
+#### Number character inclusion prompt
+![image](https://user-images.githubusercontent.com/87142377/145695084-0364cac7-575f-48cf-90d3-b47d93a1957c.png)
+
+#### Special character inclusion prompt
+![image](https://user-images.githubusercontent.com/87142377/145695188-542258f7-fc85-48ed-af43-dcf04e82ad7b.png)
+
+#### Uppercase character inclusion prompt
+![image](https://user-images.githubusercontent.com/87142377/145695207-0ef90506-2ea4-4b02-992b-93e4f23559fd.png)
+
+#### Lowercase character inclusion prompt
+![image](https://user-images.githubusercontent.com/87142377/145695217-6c21cc50-ba9f-42bc-8dc4-2327153754f1.png)
+
+#### Password result (example of 15 characters, numbers, special, upper and lower case)
+![image](https://user-images.githubusercontent.com/87142377/145695249-f57ea7ee-c85f-4b08-b1b5-dcb094479d2b.png)
+
+## Link to deployed app
+https://ericbwebdev86.github.io/password-generator/
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+let length = "";
 let special = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "]", "}", "\\", "|", ":", ":", "'", ",", "<", ".", ">", "/", "?", "`", "~"];
 let number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 let lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
@@ -6,12 +7,12 @@ let upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N
 
 function generatePassword() {
   //character length prompt
-  let length = window.prompt("Please enter desired character length. Enter a number from 8 to 128.");
+  length = window.prompt("Please enter desired character length. Enter a number from 8 to 128.");
   //while loop for password length validation
-  if (length <= 7 || length >= 129 || length !== isNaN) {
-    window.alert("Invalid entry!");
-    length = window.prompt("Please enter desired character length. Enter a number from 8 to 128.");
+  while (isNaN(length) || length < 8 || length > 128) {
+    length = window.prompt("Invalid entry. Please enter desired character length. Enter a number from 8 to 128.");
   }
+
   //password criteria prompts
   let numberCheck = window.confirm("Click the 'OK' button to include numbers in your password.");
   let specialCheck = window.confirm("Click the 'OK' button to include special characters (!, @, #, $, etc.) in your password.");
@@ -26,6 +27,7 @@ function generatePassword() {
     upperCheck = window.confirm("Click the 'OK' button to include uppercase characters in your password.");
     lowerCheck = window.confirm("Click the 'OK' button to include lowercase characters in your password.");
   }
+
   // array containing user chosen criteria
   let pwCriteria = [];
 
